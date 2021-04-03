5cf314437cbd76991b06ca6e0d36d8e2d83006d9
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-03 15:28:22 -0400

    Add and check for lintJS task

--- gulpfile.js
+++ gulpfile.js
@@ -63,9 +63,34 @@ let transpileJSForProd = () => {
         .pipe(dest(`prod/scripts`));
 };
 
+let lintJS = () => {
+    return src(`dev/scripts/*.js`)
+        .pipe(jsLinter({
+            parserOptions: {
+                ecmaVersion: 2017,
+                sourceType: `module`
+            },
+            rules: {
+                indent: [2, 4, {SwitchCase: 1}],
+                quotes: [2, `backtick`],
+                semi: [2, `always`],
+                'linebreak-style': [2, `unix`],
+                'max-len': [1, 85, 4]
+            },
+            env: {
+                es6: true,
+                node: true,
+                browser: true
+            },
+            extends: `eslint:recommended`
+        }))
+        .pipe(jsLinter.formatEach(`compact`, process.stderr));
+};
+
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
 exports.compileCSSForDev = compileCSSForDev;
 exports.compileCSSForProd = compileCSSForProd;
 exports.transpileJSForDev = transpileJSForDev;
 exports.transpileJSForProd = transpileJSForProd;
+exports.lintJS = lintJS;
