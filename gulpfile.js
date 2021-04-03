59fd59e5e8d91c77514f6977c938f791dfe4393c
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-03 15:14:22 -0400

    Add all constants to top of gulpfile.js file

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const { src, dest, series, watch } = require(`gulp`);
+const sass = require(`gulp-sass`);
+const babel = require(`gulp-babel`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const htmlValidator = require(`gulp-html`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+let browserChoice = `default`;
