1e40f120eae4b4f3c1655e877356137400b800de
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-05 18:23:39 -0400

    Add white() function to output instructions for white rice

---
+++ js/app.js
@@ -0,0 +1,3 @@
+function white() {
+    document.getElementsById(`rice`).innerHTML = `<p>Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.</p>`;
+}
