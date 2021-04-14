b901abd98bf5c9e92b86d43a5de8fd1ec02cafa4
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-14 17:04:19 -0400

    Fix spacing between sentences in rice recipy text in app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ window.onload = () => {
         let rice = document.getElementById(`rice`);
         rice.innerHTML = `<b>Making White Rice</b> <br><br>
           Combine 1 cup of rice with 2 cups of water and 1 Tbsp
-          olive oil.Bring to a boil, then reduce heat to the lowest setting.
+          olive oil. Bring to a boil, then reduce heat to the lowest setting. 
           Cook for about 18 minutes.`;
     };
 
@@ -15,10 +15,10 @@ window.onload = () => {
         rice.innerHTML = `<b>Making Sprouted California Rice</b> <br><br>
           For slightly al dente rice:
           Combine 1 1/4 cups of rice with 2 cups of water or broth and
-          1 Tbsp olive oil.Bring to a boil and stir once to mix.
+          1 Tbsp olive oil. Bring to a boil and stir once to mix.
           Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes.
           Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.
-          <br>
+          <br><br>
           For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.`;
     };
 
