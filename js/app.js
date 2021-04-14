4504fe38c2e257a2cd965c0ecf250d1cfefd1902
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-14 17:48:18 -0400

    Create variables to store value of cups of rice/water

--- js/app.js
+++ js/app.js
@@ -3,24 +3,30 @@ window.onload = () => {
     let wbutton = document.querySelector(`#button2`);
 
     let white = () => {
+        let rice_val = 1;
+        let water_val = 2;
         let rice = document.getElementById(`rice`);
         rice.innerHTML = `<b>Making White Rice</b> <br><br>
-          1 cup of rice requires 2 cups of water. <br><br>
+          cup of rice requires 2 cups of water. <br><br>
           How many cups of rice?
           <input type="number"> <br><br>
-          Combine 1 cup of rice with 2 cups of water and 1 Tbsp
+          Combine ` + rice_val + ` cup of rice with ` + water_val + 
+          ` cups of water and 1 Tbsp
           olive oil. Bring to a boil, then reduce heat to the lowest setting. 
           Cook for about 18 minutes.`;
     };
 
     let california = () => {
+        let rice_val = 1.25;
+        let water_val = 2;
         let rice = document.getElementById(`rice`);
         rice.innerHTML = `<b>Making Sprouted California Rice</b> <br><br>
           1 cup of rice requires 1.6 cups of water. <br><br>
           How many cups of rice?
           <input type="number"> <br><br>
           For slightly al dente rice:
-          Combine 1 1/4 cups of rice with 2 cups of water or broth and
+          Combine ` + rice_val + ` cups of rice with ` + water_val + 
+          ` cups of water or broth and
           1 Tbsp olive oil. Bring to a boil and stir once to mix.
           Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes.
           Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.
