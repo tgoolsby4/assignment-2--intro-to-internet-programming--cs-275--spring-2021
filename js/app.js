ea983ee7d3d9e9a294187e16d906f9f584d86a5d
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-14 16:59:47 -0400

    Udpate rice recipy titles in app.js and index.html

--- js/app.js
+++ js/app.js
@@ -4,14 +4,16 @@ window.onload = () => {
 
     let white = () => {
         let rice = document.getElementById(`rice`);
-        rice.innerHTML = `Combine 1 cup of rice with 2 cups of water and 1 Tbsp
+        rice.innerHTML = `<b>Making White Rice</b> <br><br>
+          Combine 1 cup of rice with 2 cups of water and 1 Tbsp
           olive oil.Bring to a boil, then reduce heat to the lowest setting.
           Cook for about 18 minutes.`;
     };
 
     let california = () => {
         let rice = document.getElementById(`rice`);
-        rice.innerHTML = `For slightly al dente rice:
+        rice.innerHTML = `<b>Making Sprouted California Rice</b> <br><br>
+          For slightly al dente rice:
           Combine 1 1/4 cups of rice with 2 cups of water or broth and
           1 Tbsp olive oil.Bring to a boil and stir once to mix.
           Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes.
