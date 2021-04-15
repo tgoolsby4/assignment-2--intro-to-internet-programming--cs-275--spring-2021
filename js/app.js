938448037fb382c9dd700653894e5b33d2bc6426
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-15 10:55:10 -0400

    Change recipe section in index.html from paragraph to div

--- js/app.js
+++ js/app.js
@@ -6,21 +6,21 @@ window.onload = () => {
         let rice_val = 1;
         let water_val = 2;
         let rice = document.getElementById(`rice`);
-        rice.innerHTML = `<b>Making White Rice</b> <br><br>
+        rice.innerHTML = `<p><b>Making White Rice</b> <br><br>
           1 cup of rice requires 2 cups of water. <br><br>
           How many cups of rice?
           <input type="number"> <br><br>
           Combine ` + rice_val + ` cup of rice with ` + water_val + 
           ` cups of water and 1 Tbsp
           olive oil. Bring to a boil, then reduce heat to the lowest setting. 
-          Cook for about 18 minutes.`;
+          Cook for about 18 minutes.</p>`;
     };
 
     let california = () => {
         let rice_val = 1.25;
         let water_val = 2;
         let rice = document.getElementById(`rice`);
-        rice.innerHTML = `<b>Making Sprouted California Rice</b> <br><br>
+        rice.innerHTML = `<p><b>Making Sprouted California Rice</b> <br><br>
           1 cup of rice requires 1.6 cups of water. <br><br>
           How many cups of rice?
           <input type="number"> <br><br>
@@ -31,7 +31,8 @@ window.onload = () => {
           Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes.
           Remove from heat and let stand for 5 minutes. Fluff with a fork and serve.
           <br><br>
-          For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.`;
+          For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.
+          </p>`;
     };
 
     cbutton.addEventListener(`click`, california);
