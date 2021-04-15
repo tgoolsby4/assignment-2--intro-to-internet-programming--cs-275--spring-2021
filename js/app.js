5ff60586c959c9bc24b2203c4935f6da02f2d00a
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-15 15:16:58 -0400

    Change rice functions to have innerHTML defined within for recipes

--- js/app.js
+++ js/app.js
@@ -3,13 +3,25 @@ window.onload = () => {
     let wbutton = document.querySelector(`#button2`);
 
     let white = () => {
-        document.querySelector(`#whiterice`).style.visibility = `visible`;
-        document.querySelector(`#calirice`).style.visibility = `hidden`;
+        let rice = document.querySelector(`#rice`);
+        rice.innerHTML = `<b>Making White Rice</b> <br><br>
+            1 cup of rice requires 2 cups of water. <br><br>
+            Combine 1 cup of rice with 2 cups of water and 1 Tbsp olive oil. 
+            Bring to a boil, then reduce heat to the lowest setting. 
+            Cook for about 18 minutes.`;
     };
 
     let california = () => {
-        document.querySelector(`#calirice`).style.visibility = `visible`;
-        document.querySelector(`#whiterice`).style.visibility = `hidden`;
+        let rice = document.querySelector(`#rice`);
+        rice.innerHTML = `<b>Making Sprouted California Rice</b> <br><br>
+          1 cup of rice requires 1.6 cups of water. <br><br>
+          For slightly al dente rice: Combine 1 1/4 cups of rice with 2 cups 
+          of water or broth and 1 Tbsp olive oil. 
+          Bring to a boil and stir once to mix. 
+          Reduce heat to low, cover with a tight-fitting lid and cook 
+          for 25 minutes. Remove from heat and let stand for 5 minutes. 
+          Fluff with a fork and serve. <br><br>
+          For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.`;
     };
 
     cbutton.addEventListener(`click`, california);
