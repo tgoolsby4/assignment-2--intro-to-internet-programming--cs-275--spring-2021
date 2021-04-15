cc9d29c94e1985d0555cfc82d1e5523d8c8bfed0
tgoolsby4 <tgoolsby423@gmail.com>
2021-04-15 17:04:38 -0400

    Update rice functions so that user input updates recipe in real time

--- js/app.js
+++ js/app.js
@@ -2,55 +2,54 @@ window.onload = () => {
     let cbutton = document.querySelector(`#button1`);
     let wbutton = document.querySelector(`#button2`);
     let ricecups = document.querySelector(`#cups`);
-    let ricevar;
-    let watervar;
-
-    let whitecups = () => {
-        let wcups = parseFloat(ricecups.value);
-        if (wcups < 0) {
-            alert(`Amount cannot be negative`);
-        }
-        else {
-            ricevar = wcups;
-            watervar = 2*ricevar;
-        }
-    };
-
-    let calicups = () => {
-        let ccups = parseFloat(ricecups.value);
-        if (ccups < 0) {
-            alert(`Amount cannot be negative`);
-        }
-        else {
-            ricevar = ccups;
-            watervar = 1.6*ricevar;
-        }
-    };
 
     let white = () => {
-        ricecups.addEventListener(`input`, whitecups);
         let rice = document.querySelector(`#rice`);
         rice.innerHTML = `<b>Making White Rice</b> <br><br>
             1 cup of rice requires 2 cups of water. <br><br>
-            Combine ` + ricevar + ` cup of rice with ` + watervar + ` cups of 
-            water and 1 Tbsp olive oil. 
+            Combine <output id="whitericevar"></output> cup of rice with 
+            <output id="whitewatervar"></output> 
+            cups of water and 1 Tbsp olive oil. 
             Bring to a boil, then reduce heat to the lowest setting. 
             Cook for about 18 minutes.`;
+        ricecups.addEventListener(`input`, function() {
+            let wcups = parseFloat(ricecups.value);
+            if (wcups < 0) {
+                alert(`Amount cannot be negative`);
+            }
+            else {
+                let wro = document.querySelector(`#whitericevar`);
+                let wwo = document.querySelector(`#whitewatervar`);
+                wro.value = wcups;
+                wwo.value = wcups*2;
+            }
+        });
     };
 
     let california = () => {
-        ricecups.addEventListener(`input`, calicups);
         let rice = document.querySelector(`#rice`);
         rice.innerHTML = `<b>Making Sprouted California Rice</b> <br><br>
             1 cup of rice requires 1.6 cups of water. <br><br>
-            For slightly al dente rice: Combine ` + ricevar + ` cups of 
-            rice with ` + watervar + ` cups 
+            For slightly al dente rice: Combine <output id="caliricevar"></output> 
+            cups of rice with <output id="caliwatervar"></output> cups 
             of water or broth and 1 Tbsp olive oil. 
             Bring to a boil and stir once to mix. 
             Reduce heat to low, cover with a tight-fitting lid and cook 
             for 25 minutes. Remove from heat and let stand for 5 minutes. 
             Fluff with a fork and serve. <br><br>
             For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.`;
+        ricecups.addEventListener(`input`, function() {
+            let ccups = parseFloat(ricecups.value);
+            if (ccups < 0) {
+                alert(`Amount cannot be negative`);
+            }
+            else {
+                let cro = document.querySelector(`#caliricevar`);
+                let cwo = document.querySelector(`#caliwatervar`);
+                cro.value = ccups;
+                cwo.value = ccups*1.6;
+            }
+        });
     };
 
     cbutton.addEventListener(`click`, california);
